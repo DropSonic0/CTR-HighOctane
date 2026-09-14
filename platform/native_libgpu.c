c3694b8285cd38bace49ac53f8f5107b11099b83
DropSonic <130788813+DropSonic0@users.noreply.github.com>
2026-09-14 13:47:10 -0300

    restored donwgrade

--- platform/native_libgpu.c
+++ platform/native_libgpu.c
@@ -1,8 +1,8 @@
 /*
-* Derived from REDRIVER2/PsyCross MIT source:
-* externals/PsyCross/src/psx/LIBGPU.C
-* See THIRD_PARTY_NOTICES.md for copyright and license details.
-*/
+ * Derived from REDRIVER2/PsyCross MIT source:
+ * externals/PsyCross/src/psx/LIBGPU.C
+ * See THIRD_PARTY_NOTICES.md for copyright and license details.
+ */
 
 #include <psx/libgte.h>
 #include <psx/libetc.h>
@@ -21,7 +21,7 @@
 // primitive parser and GL backend live behind this native platform boundary.
 
 int g_dbg_emulatorPaused = 0;
-void(*drawsync_callback)(void) = NULL;
+void (*drawsync_callback)(void) = NULL;
 
 #ifdef __vita__
 typedef struct
@@ -75,7 +75,7 @@ internal void NativeLibGpu_BackendUpdateVRAMTask(void *arg)
 int ClearImage(RECT16 *rect, uint8_t r, uint8_t g, uint8_t b)
 {
 #ifdef __vita__
-	NativeLibGpuClearTask task = { *rect, r, g, b };
+	NativeLibGpuClearTask task = {*rect, r, g, b};
 	NativeGpu_RunBackendTaskSync(NativeLibGpu_BackendClearTask, &task);
 #else
 	NativeRenderer_ClearVRAM(rect->x, rect->y, rect->w, rect->h, r, g, b);
@@ -136,7 +136,7 @@ int LoadImage(RECT16 *rect, void *p)
 #endif
 
 #ifdef __vita__
-	NativeLibGpuVramTask task = { r, (u16 *)p, 0, 0 };
+	NativeLibGpuVramTask task = {r, (u16 *)p, 0, 0};
 	NativeGpu_RunBackendTaskSync(NativeLibGpu_BackendLoadTask, &task);
 #else
 	NativeRenderer_CopyVRAM((unsigned short *)p, 0, 0, r.w, r.h, r.x, r.y);
@@ -158,7 +158,7 @@ int LoadImage2(RECT16 *rect, void *p)
 int MoveImage(RECT16 *rect, int x, int y)
 {
 #ifdef __vita__
-	NativeLibGpuVramTask task = { *rect, NULL, x, y };
+	NativeLibGpuVramTask task = {*rect, NULL, x, y};
 	NativeGpu_RunBackendTaskSync(NativeLibGpu_BackendMoveTask, &task);
 #else
 	NativeRenderer_CopyVRAM(NULL, rect->x, rect->y, rect->w, rect->h, x, y);
@@ -169,7 +169,7 @@ int MoveImage(RECT16 *rect, int x, int y)
 int StoreImage(RECT16 *rect, uint32_t *p)
 {
 #ifdef __vita__
-	NativeLibGpuVramTask task = { *rect, (u16 *)p, 0, 0 };
+	NativeLibGpuVramTask task = {*rect, (u16 *)p, 0, 0};
 	NativeGpu_RunBackendTaskSync(NativeLibGpu_BackendReadTask, &task);
 #else
 	NativeRenderer_ReadVRAM((unsigned short *)p, rect->x, rect->y, rect->w, rect->h);
@@ -340,18 +340,12 @@ DRAWENV *SetDefDrawEnv(DRAWENV *env, int x, int y, int w, int h)
 
 void SetDrawEnv(DR_ENV *dr_env, DRAWENV *env)
 {
-	u32 c0 = ((env->clip.y & 0x3FF) << 10) | (env->clip.x & 0x3FF) | 0xE3000000;
-	u32 c1 = (((env->clip.y + env->clip.h - 1) & 0x3FF) << 10) | ((env->clip.x + env->clip.w - 1) & 0x3FF) | 0xE4000000;
-	u32 c2 = ((env->ofs[1] & 0x7FF) << 11) | (env->ofs[0] & 0x7FF) | 0xE5000000;
-	u32 c3 = 32 * (((256 - env->tw.h) >> 3) & 0x1F) | (((256 - env->tw.w) >> 3) & 0x1F) | (((env->tw.y >> 3) & 0x1F) << 15) |
-		(((env->tw.x >> 3) & 0x1F) << 10) | 0xE2000000;
-	u32 c4 = ((env->dtd != 0) << 9) | ((env->dfe != 0) << 10) | (env->tpage & 0x1FF) | 0xE1000000;
-
-	CTR_WriteU32LE(&dr_env->code[0], c0);
-	CTR_WriteU32LE(&dr_env->code[1], c1);
-	CTR_WriteU32LE(&dr_env->code[2], c2);
-	CTR_WriteU32LE(&dr_env->code[3], c3);
-	CTR_WriteU32LE(&dr_env->code[4], c4);
+	dr_env->code[0] = ((env->clip.y & 0x3FF) << 10) | (env->clip.x & 0x3FF) | 0xE3000000;
+	dr_env->code[1] = (((env->clip.y + env->clip.h - 1) & 0x3FF) << 10) | ((env->clip.x + env->clip.w - 1) & 0x3FF) | 0xE4000000;
+	dr_env->code[2] = ((env->ofs[1] & 0x7FF) << 11) | (env->ofs[0] & 0x7FF) | 0xE5000000;
+	dr_env->code[3] = 32 * (((256 - env->tw.h) >> 3) & 0x1F) | (((256 - env->tw.w) >> 3) & 0x1F) | (((env->tw.y >> 3) & 0x1F) << 15) |
+	                  (((env->tw.x >> 3) & 0x1F) << 10) | 0xE2000000;
+	dr_env->code[4] = ((env->dtd != 0) << 9) | ((env->dfe != 0) << 10) | (env->tpage & 0x1FF) | 0xE1000000;
 
 	setlen(dr_env, 5);
 }
@@ -365,25 +359,23 @@ void SetDrawMove(DR_MOVE *p, RECT16 *rect, int x, int y)
 		len = 0;
 	}
 
-	CTR_WriteU32LE(&p->code[0], 0x01000000);
-	CTR_WriteU32LE(&p->code[1], 0x80000000);
-	CTR_WriteU32LE(&p->code[2], ((u32)(u16)rect->x) | ((u32)(u16)rect->y << 16));
-	CTR_WriteU32LE(&p->code[3], (y << 0x10) | (x & 0xffffU));
-	CTR_WriteU32LE(&p->code[4], ((u32)(u16)rect->w) | ((u32)(u16)rect->h << 16));
+	p->code[0] = 0x1000000;
+	p->code[1] = 0x80000000;
+	p->code[2] = ((u32)(u16)rect->x) | ((u32)(u16)rect->y << 16);
+	p->code[3] = (y << 0x10) | (x & 0xffffU);
+	p->code[4] = ((u32)(u16)rect->w) | ((u32)(u16)rect->h << 16);
 
 	setlen(p, len);
 }
 
 void SetPsyXTexture(DR_PSYX_TEX *p, uint32_t grTextureId, int width, int height)
 {
-	u32 c0 = 0xB1000000u | (grTextureId & 0x00ffffffu);
-	u32 c1 = ((uint32_t)(height & 0x0fff) << 16) | (uint32_t)(width & 0x0fff);
-	CTR_WriteU32LE(&p->code[0], c0);
-	CTR_WriteU32LE(&p->code[1], c1);
+	p->code[0] = 0xB1000000u | (grTextureId & 0x00ffffffu);
+	p->code[1] = ((uint32_t)(height & 0x0fff) << 16) | (uint32_t)(width & 0x0fff);
 	setlen(p, 2);
 }
 
-uint32_t DrawSyncCallback(void(*func)(void))
+uint32_t DrawSyncCallback(void (*func)(void))
 {
 	drawsync_callback = func;
 	return 0;
